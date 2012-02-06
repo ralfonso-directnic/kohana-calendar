0a4d206b360814af8b2157f31762758b8f1fddd9
David Pommer <pommer@code8.de>
2012-02-06 22:12:13 +0100

    Event_Observer and Event_Subject implements their SPL interfaces

--- classes/kohana/event/observer.php
+++ classes/kohana/event/observer.php
@@ -9,7 +9,7 @@
  * @copyright  (c) 2007-2008 Kohana Team
  * @license    http://kohanaphp.com/license.html
  */
-abstract class Kohana_Event_Observer {
+abstract class Kohana_Event_Observer implements SplObserver {
 
 	// Calling object
 	protected $caller;
--- classes/kohana/event/subject.php
+++ classes/kohana/event/subject.php
@@ -9,7 +9,7 @@
  * @copyright  (c) 2007-2008 Kohana Team
  * @license    http://kohanaphp.com/license.html
  */
-abstract class Kohana_Event_Subject {
+abstract class Kohana_Event_Subject implements SplSubject {
 
 	// Attached subject listeners
 	protected $listeners = array();
